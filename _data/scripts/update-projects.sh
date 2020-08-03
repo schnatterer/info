6dcc39ef92eae7d3d360d3ce73094a8f794c2345
Johannes Schnatterer <johannes@schnatterer.info>
2020-08-03 16:36:22 +0200

    Work around failure during project update

--- _data/scripts/update-projects.sh
+++ _data/scripts/update-projects.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
-set -o errexit -o nounset -o pipefail
+set -o nounset -o pipefail
+# Leads to mysterious failures after firstDate=... 
+#set -o errexit 
 
+set -x
 BASEDIR=$(dirname $0)
 
 i=0
 for project in $(yq -r '.projects[].name' ${BASEDIR}/../projects.yaml); do
-  git clone --bare http://github.com/${project} tmp
-  cd tmp
+  tmpdir=$(mktemp -d)
+  git clone --bare http://github.com/${project} ${tmpdir}
+  cd ${tmpdir}
   firstDate="$(git log --all --author='schnatterer' --reverse --pretty=format:'%as' | head -n1)"
   lastDate="$(git log --all --author='schnatterer' --pretty=format:'%as' | head -n1)"
   nCommits="$(git log --all --author='schnatterer' --pretty=format:'%as' | wc -l)"
-  cd ..
+  cd -
+  rm -rf ${tmpdir}
   echo ${i}: ${project}, commits from ${firstDate} to ${lastDate}
   tmpfile=$(mktemp)
   yq --arg val ${firstDate} ".projects[$i].commits.from=\$val" ${BASEDIR}/../projects.yaml \
     | jq --arg val ${lastDate} ".projects[$i].commits.to=\$val" \
     | yq --arg val ${nCommits} --yaml-output ".projects[$i].commits.amount=\$val" > ${tmpfile}
-  mv ${tmpfile} ${BASEDIR}/../projects.yaml
+  mv "${tmpfile}" "${BASEDIR}/../projects.yaml"
   rm -rf tmp
   i=$((i+1))
 done
