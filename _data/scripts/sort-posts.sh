801dc2bf2b06dda87411f7ec6ad89d9fd0c99edc
Johannes Schnatterer <johannes@schnatterer.info>
2020-08-03 16:34:50 +0200

    Make sorting posts deterministic.
    Two post on same date lead to potentially different result on each call.

--- _data/scripts/sort-posts.sh
+++ _data/scripts/sort-posts.sh
@@ -5,5 +5,5 @@ set -o errexit -o nounset -o pipefail
 BASEDIR=$(dirname $0)
 
 tmpfile=$(mktemp)
-yq --yaml-output  ".[].pubs |= (sort_by(.date) | reverse)" ${BASEDIR}/../publications.yaml > ${tmpfile}
+yq --yaml-output  ".[].pubs |= (sort_by(.date, .title) | reverse)" ${BASEDIR}/../publications.yaml > ${tmpfile}
 mv ${tmpfile} ${BASEDIR}/../publications.yaml
